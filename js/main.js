6d5e61e1f70bad8b20a302172e2ff327a220993b
Juan Diego Vasquez <juandvasquezd@gmail.com>
2023-01-31 15:36:07 -0300

    Ajustes a .js para envolver comportamiento en funciones, en lugar de dispararlo línea a línea. Cambios en textos. Padding index.

--- js/main.js
+++ js/main.js
@@ -2,25 +2,33 @@
 Un poco abstracta la idea de esta preentrega,
 pero aquí vamos
 */
+function imprimeGrilla(filas, columnas) {
+    console.log(`Perfecto, hagamos una grilla de flashcards de ${filas}x${columnas}`);
 
-alert("¡Hola! Vamos a dibujar una grilla en la consola simulando un tablero o mesa con flashcards. Presiona para continuar.");
-
-let numFilas = parseInt(prompt("¿Cuántas filas tendrá la grilla? Mínimo 2, máximo 8"));
-while (!numFilas || !(numFilas >= 2 && numFilas <= 8)) {
-    numFilas = parseInt(prompt("Valor no válido. Intentemos de nuevo: ¿cuántas filas tendrá la grilla? Mínimo 2, máximo 8"));
-}
-
-let numColumnas = parseInt(prompt(`Perfecto, son ${numFilas} filas. ¿Y cuántas columnas?`));
-while (!numColumnas || !(numColumnas >= 2 && numColumnas <= 8)) {
-    numColumnas = parseInt(prompt("Valor no válido. Intentemos de nuevo: ¿cuántas columnas tendrá la grilla? Mínimo 2, máximo 8"));
-}
-
-console.log(`Perfecto, hagamos una grilla de flashcards de ${numFilas}x${numColumnas}`);
-let textoFila = "";
-for (let i = 0; i < numFilas; i++) {
-    for (let j = 0; j < numColumnas; j++) {
-        textoFila += "🟩 ";
+    let textoFila = "";
+    for (let i = 0; i < filas; i++) {
+        for (let j = 0; j < columnas; j++) {
+            textoFila += "🟩 ";
+        }
+        console.log(`${(i + 1)}: ${textoFila} `);
+        textoFila = "";
     }
-    console.log(`${(i + 1)}: ${textoFila} `);
-    textoFila = "";
 }
+
+function generaGrilla() {
+    alert("¡Hola! Vamos a dibujar una grilla en la consola simulando un tablero o mesa con flashcards. Presiona para continuar.");
+
+    let numFilas = parseInt(prompt("¿Cuántas filas tendrá la grilla? Mínimo 2, máximo 8"));
+    while (!numFilas || !(numFilas >= 2 && numFilas <= 8)) {
+        numFilas = parseInt(prompt("Valor no válido. Intentemos de nuevo: ¿cuántas filas tendrá la grilla? Mínimo 2, máximo 8"));
+    }
+
+    let numColumnas = parseInt(prompt(`Perfecto, son ${numFilas} filas. ¿Y cuántas columnas?`));
+    while (!numColumnas || !(numColumnas >= 2 && numColumnas <= 8)) {
+        numColumnas = parseInt(prompt("Valor no válido. Intentemos de nuevo: ¿cuántas columnas tendrá la grilla? Mínimo 2, máximo 8"));
+    }
+
+    imprimeGrilla(numFilas, numColumnas);
+}
+
+generaGrilla();
