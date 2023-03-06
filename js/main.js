34ef2aece7119c8ce7b5d2ae21548ed473022839
Juan Diego Vasquez <juandvasquezd@gmail.com>
2023-03-05 23:40:35 -0300

    Funcionalidad básica para mostrar un verbo a la vez y recordar dónde quedó el usuario. Botones para avanzar y retroceder.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,74 @@
+/* Definiciones generales */
 import { verbos } from "./verbs.js";
 const dificultades = 5;
 const longPorDefecto = 20;
-let listaActual = [];
+const dificultadPorDefecto = 2;
 let textoFrente = "";
 let textoAtras = "";
-let textoDesafio = "";
 
+
+/* Elementos */
 const cardFront = document.getElementById("cardFront");
 const cardBack = document.getElementById("cardBack");
+const overlaySettings = document.getElementById("mainOverlay");
+const formularioSettings = document.getElementById("settingsForm");
+const selectorDificultad = document.getElementById("selectorDificultad");
+const selectorCantidad = document.getElementById("cantidadEscogida");
+const inputsActualiza = document.getElementById("actualizaPosicion").elements;
+const reinicia = document.getElementById("reinicia");
 
-const randomizaLista = lista => {
-    for (let i = lista.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [lista[i], lista[j]] = [lista[j], lista[i]];
-        // StackOverflow totalmente, para evitar sesgos de órdenes no-tan-random
+
+/* ¿Sesión existente? */
+let sesion = JSON.parse(localStorage.getItem("sesion")) || false;
+let listaActual = JSON.parse(localStorage.getItem("listaActual")) || [];
+let posicionActual = parseInt(localStorage.getItem("posicionActual")) || 0;
+
+
+/* Funciones */
+const iniciaApp = () => {
+    if (!sesion || !listaActual) {
+        overlaySettings.className = "main-overlay show";
+        selectorDificultad.max = dificultades;
+        selectorDificultad.value = 0;
+        selectorCantidad.max = verbos.length;
+    } else {
+        imprimeTarjetaActual(posicionActual);
     }
-    return lista;
+}
+
+const defineSesion = settings => {
+    let cantidad = settings.get("cantidad");
+    if (!cantidad || cantidad < 1) {
+        cantidad = longPorDefecto;
+    } else if (cantidad > verbos.length) {
+        cantidad = verbos.length;
+    }
+
+    let dificultad = settings.get("dificultad");
+    if (!dificultad || dificultad < 1) {
+        dificultad = dificultadPorDefecto;
+    } else if (dificultad > dificultades) {
+        dificultad = dificultades;
+    }
+
+    let listaGenerada = generaLista(cantidad, dificultad);
+    let listaRandomizada = randomizaLista(listaGenerada);
+
+    let sesion = { cantidad, dificultad }
+    let sesionJson = JSON.stringify(sesion);
+    let listaJson = JSON.stringify(listaRandomizada);
+    localStorage.setItem("sesion", sesionJson);
+    localStorage.setItem("listaActual", listaJson);
+    listaActual = listaRandomizada;
+    localStorage.setItem("posicionActual", "0");
+
+    overlaySettings.className = "main-overlay";
+    imprimeTarjetaActual(0);
 }
 
 const generaLista = (cantidadVerbos, dificultad) => {
     let puntoMedio;
-    if (!dificultad || dificultad < 1) {
-        dificultad = 1;
-    } else if (dificultad > dificultades) {
-        dificultad = dificultades;
-    }
     puntoMedio = Math.floor(((dificultad - 1) / (dificultades - 1)) * verbos.length);
-
-    if (!cantidadVerbos || cantidadVerbos < 1) {
-        cantidadVerbos = longPorDefecto;
-    } else if (cantidadVerbos > verbos.length) {
-        cantidadVerbos = verbos.length;
-    }
 
     let limiteInferior = puntoMedio - Math.floor(cantidadVerbos / 2);
     let limiteSuperior = puntoMedio + Math.ceil(cantidadVerbos / 2) - 1;
@@ -44,52 +81,89 @@ const generaLista = (cantidadVerbos, dificultad) => {
         limiteSuperior = verbos.length - 1;
     }
 
-    textoFrente = `<p class="p-b025">${cantidadVerbos} verbos, dificultad: ${dificultad}/${dificultades}</p>`;
-    textoAtras = `<p class="p-b025">${cantidadVerbos} verbos, dificultad: ${dificultad}/${dificultades}</p>`;
-    textoDesafio = `Practicando ${cantidadVerbos} verbos. Dificultad: ${dificultad}/${dificultades}\n`;
     return verbos.slice(limiteInferior, limiteSuperior + 1);
 }
 
-const cumpleConDesafio2 = (lista, elemento) => {
-    /*  Sé que no era necesario manejar el DOM para este desafío,
-    pero era más fácil validar todo visualmente así.
-
-    Esta función en sí no será necesaria más adelante, ya que la flashcard tendrá un
-    solo verbo de la lista generada a la vez; su trabajo es solo cumplir con la consigna */
-    let verboAValidar = elemento.replace("to", ""); // Estandaricemos
-    verboAValidar = `to ${verboAValidar.trim().toLowerCase()}`; // Estandaricemos más
-
-    for (let verbo of lista) {
-        textoFrente += `
-        <p class="p-b025">
-            Verb <strong>${verbo.infinitive}</strong> &gt; simple past: <strong>${verbo.simplePast}</strong>
-        </p>`;
-
-        textoAtras += `
-        <p class="p-b025">
-            Verb <strong>${verbo.infinitive}</strong> &gt; past participle: <strong>${verbo.pastParticiple}</strong>
-        </p>`;
-
-        textoDesafio += `Verb ${verbo.infinitive} > simple past: ${verbo.simplePast}\n`;
+const randomizaLista = lista => {
+    for (let i = lista.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [lista[i], lista[j]] = [lista[j], lista[i]];
     }
-
-    const verboEncontrado = lista.find((verbo) => verbo.infinitive === verboAValidar);
-    const mensajeEncontrado = !verboEncontrado
-        ? `El verbo ${verboAValidar} no está en la lista\n`
-        : `El verbo ${verboAValidar} sí está en la lista\n`;
-
-    cardFront.innerHTML += textoFrente;
-    cardBack.innerHTML += textoAtras;
-    console.log(mensajeEncontrado, textoDesafio);
+    return lista;
 }
 
-listaActual = generaLista(
-    parseInt(prompt(`Cantidad de verbos a practicar (máx. ${verbos.length} o ${longPorDefecto} por defecto)`)),
-    parseInt(prompt(`Dificultad 1-${dificultades}`))
-);
-listaActual = randomizaLista(listaActual);
+const imprimeTarjetaActual = (pos) => {
+    let verbo = listaActual[pos];
 
-cumpleConDesafio2(
-    listaActual,
-    prompt("Finalmente, ingresa un verbo en presente para validar si está en la lista a practicar")
-);
+    textoFrente = `
+        <h2 class="card-title p-b025">Verb: ${verbo.infinitive}</h2>
+        <p class="i p-b">${verbo.infinitiveExtraInfo ? verbo.infinitiveExtraInfo : "&nbsp;"}</p>
+        <p class="p-b025">
+            Verbo ${pos + 1} de ${listaActual.length}
+        </p>`;
+
+    textoAtras = `
+        <h2 class="card-title p-b025">Verb: ${verbo.infinitive}</h2>
+        <p class="i p-b">${verbo.infinitiveExtraInfo ? verbo.infinitiveExtraInfo : "&nbsp;"}</p>
+
+        <h3 class="b">${verbo.simplePast}</h3>
+        <p class="p-b025">simple past</p>
+        <p class="i p-b"> ${verbo.simplePastExtraInfo} </p>
+        
+        <h3 class="b">${verbo.pastParticiple}</h3>
+        <p class="p-b025">past participle</p>
+        <p class="i p-b"> ${verbo.pastParticipleExtraInfo} </p>`;
+
+    cardFront.innerHTML = textoFrente;
+    cardBack.innerHTML = textoAtras;
+}
+
+const actualizaPosicion = direccion => {
+    switch (direccion) {
+        case "siguiente":
+            posicionActual = posicionActual < (listaActual.length - 1)
+                ? posicionActual + 1
+                : listaActual.length - 1;
+            break;
+        case "anterior":
+            posicionActual = posicionActual > 0
+                ? posicionActual - 1
+                : 0;
+            break;
+        case "inicio":
+            posicionActual = 0;
+            break;
+        case "final":
+            posicionActual = listaActual.length - 1;
+        default:
+            posicionActual = posicionActual < (listaActual.length - 1)
+                ? posicionActual + 1
+                : listaActual.length - 1;
+    }
+
+    imprimeTarjetaActual(posicionActual);
+    localStorage.setItem("posicionActual", `${posicionActual}`);
+}
+
+const borraSesion = () => {
+    localStorage.clear();
+    window.location.reload();
+}
+
+
+/* Listeners y ejecuciones */
+document.addEventListener("DOMContentLoaded", iniciaApp);
+
+formularioSettings.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(settingsForm);
+    defineSesion(data)
+});
+
+for (let i = 0; i < inputsActualiza.length; i++) {
+    inputsActualiza[i].addEventListener("click", () => {
+        actualizaPosicion(inputsActualiza[i].value);
+    });
+}
+
+reinicia.addEventListener("click", borraSesion);
